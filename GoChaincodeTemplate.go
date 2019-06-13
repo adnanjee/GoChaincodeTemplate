ad2b3da486129aa5102ed308acbbbd4fa9493c38
Adnan Iftekhar <adnan.iftekhar.ch@gmail.com>
2019-06-14 03:16:00 +0800

    add logging function

--- GoChaincodeTemplate.go
+++ GoChaincodeTemplate.go
@@ -4,6 +4,11 @@ import "fmt"
 import "github.com/hyperledger/fabric/core/chaincode/shim" //The shim package
 import "github.com/hyperledger/fabric/protos/peer" //peer.response is in the peer package
 
+//Create an instance of the Logger
+
+const ChaincodeName = "GoChaincodeTemplate"
+var logger = shim.NewLogger(ChaincodeName)
+
 //Tokenchaincode represent our chaincode object
 type TokenChaincode struct {
 
@@ -13,13 +18,21 @@ type TokenChaincode struct {
 //Init Implemets the Init method
 func (token *TokenChaincode) Init(stub shim.ChaincodeStubInterface) peer.Response{
 	fmt.Println("Init Executed")
-	return shim.Success(nil)
+	
+	logger.Debug("Init executed - DEBUG") //debugging message outputs
+	return shim.Success(nil) //return success
 }
 
 // Invoke method
 func (token *TokenChaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response{
 	fmt.Println("Invoke succeed")
 
+	//emit four types of meassages for logging
+	logger.Debug("DEBUG= Invoke Executed")
+	logger.Info("INFO: Invoke Executed")
+	logger.Noticef("NOTICE: format string value=%s", "Invoke Executed")
+	logger.Warning("WARNING=Invoke Executed", "[any number of parameters of different types")
+	
 	//return success function
 	payload := []byte("This the payload")
 	return shim.Success(payload)
